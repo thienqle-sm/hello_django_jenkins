50f9d7efc839fd081e885cb475abfa41f2fc5921
thien-sm <thien@summitworks.com>
2021-07-06 14:38:13 -0400

    test about page

--- hello_project/test.py
+++ hello_project/test.py
@@ -8,4 +8,4 @@ class HomePageTests(SimpleTestCase):
 
     def test_about_page_status_code(self):
         response = self.client.get('/about')
-        self.assertEquals(response.status_code,404)
+        self.assertEquals(response.status_code,200)
