14f39d82c65611aaee74ac337cea57e7c74d08a7
thien-sm <thien@summitworks.com>
2021-07-06 14:13:49 -0400

    first commit

---
+++ hello_project/test.py
@@ -0,0 +1,11 @@
+from django.test import SimpleTestCase
+
+class HomePageTests(SimpleTestCase):
+
+    def test_admin_page_status_code(self):
+        response = self.client.get('/admin')
+        self.assertEquals(response.status_code,301)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about')
+        self.assertEquals(response.status_code,404)
